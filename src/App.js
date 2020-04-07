2ee8c45b1c7a58e09a00a77ffb2ff3c960064902
anvar <ishbayev16@itu.edu.tr>
2020-04-07 04:26:16 +0300

    ROUTING to hats page withRouter Example

--- src/App.js
+++ src/App.js
@@ -3,25 +3,10 @@ import './App.css';
 import Homepage from './pages/Homepage';
 import {Route, Switch, Link} from 'react-router-dom';
 
-const HatsPage = (props) => {
-return(
-  <div>
-    <Link to='/sneakers/12'>sneakers by link</Link>
-    <br/>
-    <button onClick={()=> props.history.push('/sneakers/34')}>sneakers by button</button>
-    <h1>Hats page</h1>
-
-  </div>
-)
-}
-
-const SneakersPage = (props) => {
-  console.log(props);
+const HatsPage = () =>{
   return(
-  <div>
-    <h1>Sneakers page: {props.match.params.sneakerId}</h1>
-  </div>
-)
+      <div>hast page</div>
+  )
 }
 
 function App() {
@@ -30,7 +15,6 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/hats' component={HatsPage}/>
-        <Route path='/sneakers/:sneakerId' component={SneakersPage}/>
       </Switch>
       
     </div>
